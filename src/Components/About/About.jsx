216475ebaa84fc3cb6e17d637aaa0b2ec44779e5
jameswquinn <jameswquinn@me.com>
2020-05-30 17:35:04 +0100

    initial commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,38 @@
+/** @jsx h */
+import { h, Component, Fragment } from "preact";
+import Helmet from "preact-helmet";
+
+import * as timeago from "timeago.js";
+import lozad from "lozad";
+
+export default class About extends Component {
+  componentDidMount() {
+    const observer = lozad(".lazy", {
+      loaded: function(el) {
+        // Custom implementation on a loaded element
+        el.classList.add("is-loaded");
+      },
+      rootMargin: "10px 0px", // syntax similar to that of CSS Margin
+      threshold: 0.4 // ratio of element convergence
+    });
+    observer.observe();
+  }
+
+  componentDidUpdate() {}
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    
+    return (
+      <Fragment>
+        <Helmet title="My My My Title Hay James Yet Another Page" />
+        <article>
+            <h1>About Page</h1>
+        </article>
+      </Fragment>
+    );
+  }
+}
